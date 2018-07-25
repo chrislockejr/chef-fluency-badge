0bcbebd6b2c3516d5e85240ab1be79f648a0f17f
Chris <chris.locke.jr@gmail.com>
2018-07-25 19:22:41 +0000

    updating apache cookbook to 2.0

--- cookbooks/apache/recipes/websites.rb
+++ cookbooks/apache/recipes/websites.rb
@@ -1,10 +1,10 @@
 file 'default www' do
 	path '/var/www/html/index.html'
-	content 'Hello World!'
+	content 'Hello World! v2.0'
 end
 
-webnodes = search('node', 'role:web')
+#webnodes = search('node', 'role:web')
 
-webnodes.each do |node|
-	puts node
-end
+#webnodes.each do |node|
+#	puts node
+#end
